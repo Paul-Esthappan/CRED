75cfdc51f8686e006ae46d5475a1e4517d7cd588
paul-esthappan <paulesthappan@gmil.com>
2023-07-19 13:18:17 +0530

    1st

---
+++ cred/src/Data/Data.js
@@ -0,0 +1,8 @@
+
+
+
+const data1=[
+    {name:"Tom", age:18, course:"Java"},
+    {name:"John", age:40, course:"Php"},
+    {name:"Soman", age:30, course:"MERn"}
+]
---
+++ cred/src/Pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+function Home() {
+    let data={name:"TimeRanges"}
+    const data1=[{name:"Tom", age:18},{name:"John", age:40}]
+  return (
+    <div>
+        <p>sfa</p>
+        
+     
+            <table>
+                <thead>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Course</th>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+                 
+    </div>
+  )
+}
+
+export default Home
